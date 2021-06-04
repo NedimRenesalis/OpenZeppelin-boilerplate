3c4185e9b5cc2c417f940fc96932d79030ecf580
Nedim Begić <nedim@renesalis-packaging.com>
2021-06-04 13:16:13 +0200

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -2,3 +2,10 @@
 async function main() {
   // Here I will put the code for interacting.
 }
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
