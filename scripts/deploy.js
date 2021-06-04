a78d662943085b645bb0b1c71338f1811a3713d3
Nedim Begić <nedim@renesalis-packaging.com>
2021-06-04 11:11:16 +0200

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,3 +3,7 @@ async function main() {
 // We get the contract to deploy
   const Box = await ethers.getContractFactory("Box");
   console.log("Deploying Box...");
+const box = await Box.deploy();
+  await box.deployed();
+  console.log("Box deployed to:", box.address);
+}
