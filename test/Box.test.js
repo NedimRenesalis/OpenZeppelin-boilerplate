190a84bd5a0e35ef8ae0e347a0b1c99e069d4c69
Nedim Begić <nedim@renesalis-packaging.com>
2021-06-04 16:00:40 +0200

    Test if the returned value is the same one

--- test/Box.test.js
+++ test/Box.test.js
@@ -17,3 +17,9 @@ describe('Box', function () {
   it('retrieve returns a value previously stored', async function () {
     // Store a value
     await this.box.store(42);
+
+// Test if the returned value is the same one
+    // Note that we need to use strings to compare the 256 bit integers
+    expect((await this.box.retrieve()).toString()).to.equal('42');
+  });
+});
