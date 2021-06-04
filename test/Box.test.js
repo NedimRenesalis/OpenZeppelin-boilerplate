5581e6da3b54825348056357384c3dcebcb58e6b
Nedim Begić <nedim@renesalis-packaging.com>
2021-06-04 15:58:25 +0200

    update

--- test/Box.test.js
+++ test/Box.test.js
@@ -1,3 +1,14 @@
 // test/Box.test.js
 // Load dependencies
 const { expect } = require('chai');
+
+// Start test block
+describe('Box', function () {
+  before(async function () {
+    this.Box = await ethers.getContractFactory("Box");
+  });
+
+ beforeEach(async function () {
+    this.box = await this.Box.deploy();
+    await this.box.deployed();
+  });
