122b2f3921eaf7cc25c147012b173e2ec4f7488c
Nedim Begić <nedim@renesalis-packaging.com>
2021-06-04 10:08:01 +0200

    update

--- contracts/access-control/Auth.sol
+++ contracts/access-control/Auth.sol
@@ -4,3 +4,13 @@ pragma solidity ^0.6.0;
 
 contract Auth {
     address private administrator;
+
+ constructor() public {
+        // Make the deployer of the contract the administrator
+        administrator = msg.sender;
+    }
+
+    function isAdministrator(address user) public view returns (bool) {
+        return user == administrator;
+    }
+}
