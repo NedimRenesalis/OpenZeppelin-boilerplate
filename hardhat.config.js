ebad662b595879e1bf32ab3a0a6f56e861956dee
Nedim Begić <nedim@renesalis-packaging.com>
2021-06-05 14:23:41 +0200

    Adding ropsten testnet to hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,5 +7,11 @@ const { alchemyApiKey, mnemonic } = require('./secrets.json');
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.0",
+ networks: {
+ropsten: {
+url: `https://eth-ropsten.alchemyapi.io/v2/${alchemyApiKey}`,
+accounts: {mnemonic: mnemonic}
+}
+}, 
+ solidity: "0.8.0",
 };
