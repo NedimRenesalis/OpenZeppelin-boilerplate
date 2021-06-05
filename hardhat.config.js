9417765c4e87feabf45406d7b5bdf7c902afea73
Nedim Begić <nedim@renesalis-packaging.com>
2021-06-05 14:00:24 +0200

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,8 @@
 // hardhat.config.js
 require('@nomiclabs/hardhat-ethers');
 require("@nomiclabs/hardhat-truffle5");
+
+const { alchemyApiKey, mnemonic } = require('./secrets.json');
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
